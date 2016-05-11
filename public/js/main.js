ca735f73b666c38078dc879f040f05a232460337
Журавлев Павел Игоревич <zhuravlev@ek.apress.ru>
2016-05-11 17:24:00 +0500

    Slider Work

--- public/js/main.js
+++ public/js/main.js
@@ -185,15 +185,22 @@ $(document).ready(function(){
 				settings         = $.extend({
 			      activeClass    : 'slider-active',
 			      activePos      : 0,
-			      timeStep       : 3000,
+			      timeStep       : 7000,
 			      slideWidth     : $arrSlides.outerWidth(),
 			      arrows         : true
 			    }, options),
 			    slideWidth       = settings.slideWidth, 
-			    indexActiveSlide = settings.activePos,
-			    slideStartIndex  = 1,
+			    indexActiveSlide = settings.activePos + 2,
+			    slideStartIndex  = 2,
 			    slideEndIndex,
 			    inter;
+
+			function cancelClick(){
+				$('body').addClass('body-bg');
+				setTimeout(function(){
+					$('body').removeClass('body-bg');
+				}, 500);
+			}
 
 			this.addArrows = function(){
 				if(settings.arrows){
@@ -203,7 +210,6 @@ $(document).ready(function(){
 					);
 				}
 			}
-
 
 			this.clearAttrs = function($elem){
 				return $elem.removeAttr('data-item');
@@ -222,65 +228,89 @@ $(document).ready(function(){
 			}
 
 			this.setActiveSlide = function(){
-				$arrSlides.eq(settings.activePos).addClass(settings.activeClass);
-
-				$arrSlides    = $slider.children();
+				$arrSlides = $slider.children();
+				
+				$slider.children('*[data-item="'+ settings.activePos +'"]').addClass(settings.activeClass);
+				$slider.move(indexActiveSlide);
+				
 				countSlides   = $arrSlides.length - 1;
-				slideEndIndex = countSlides - 3;
+				slideEndIndex = countSlides - 1;
 			}
 
-			this.moveToActive = function(data){
-				// var toSlide = $slider.find('.slider-active').index() + 1;
-				var toSlide = $arrSlides.eq(indexActiveSlide).index() + 2;
-				console.log(toSlide);
+			this.getIndexActiveSlide = function(){
+				return $slider.children('.' + settings.activeClass).index();
+			}
 
+			this.changeActiveSlide = function(nextSlide){
+				$arrSlides.siblings().removeClass(settings.activeClass);
+				$arrSlides.eq(nextSlide).addClass(settings.activeClass);
+			}
+
+			this.invisibleMoveSlider = function(indexPosition, movingPosition){
+				$slider.move(indexPosition, function(){
+					$slider.css({
+						'left': -slideWidth * movingPosition
+					});
+					$slider.changeActiveSlide(movingPosition);
+				});
+			}
+
+			this.checkSlide = function(dataSlide){
+				var dataSlide = dataSlide || 1,
+					nextSlide = $slider.getIndexActiveSlide() + dataSlide;
+
+				if(nextSlide == slideEndIndex){
+					$slider.invisibleMoveSlider(nextSlide, slideStartIndex);
+				}
+				else if(nextSlide == (slideStartIndex-1)){
+					$slider.invisibleMoveSlider(nextSlide, slideEndIndex-1);	
+				}
+				else {
+					$slider.move(nextSlide);
+					$slider.changeActiveSlide(nextSlide);
+				}	
+			}
+
+			this.move = function(indexPos, callback){
 				$slider.transition({
-					'left': -slideWidth * toSlide
-				}, function(){
-					if(indexActiveSlide == slideEndIndex){
-						$slider.css({
-							'left': -slideWidth * 2
-						});
-						indexActiveSlide = slideStartIndex;	
-					}
-					else indexActiveSlide++;
-				});
+					'left': -slideWidth * indexPos
+				}, 500, function(){
+					if(callback && typeof callback == "function") callback();
+				});	
 			}
 
-			this.setActive = function(callback){
-				$arrSlidesDef.siblings().removeClass(settings.activeClass);
-				$arrSlidesDef.eq(indexActiveSlide).addClass(settings.activeClass);
-
-				if(callback && typeof callback == "function") callback();
-			}
-
-			this.intervalStart_Stop = function(interval, action, func){ // action = stop, action = start
-				if(action == "stop"){
-					clearInterval(interval);
-				}
-				else if(action == "start"){
-					interval = setInterval(func, settings.timeStep);
-				}
-			}
-
-			this.actionInterval = function(){
-				$slider.setActive(function(){
-					$slider.moveToActive();
-				});
+			this.startTimer = function(timer, func){
+				return setInterval(function(){
+							$slider.checkSlide();
+						}, settings.timeStep);
 			}
 
 			this.arrowClickHandler = function(){
-				
+				$(document).on('click', '.slider-arrow', function(){
+					var dataSlide = parseInt($(this).data('slide'));
+					clearInterval(inter);
+
+					cancelClick();
+					$slider.checkSlide(dataSlide);
+
+					inter = $slider.startTimer(inter);
+
+					return false;
+				});
 			}
 
 			this.initSlider = function(){
+				 
+				if((settings.activePos > $arrSlidesDef.length) || (settings.activePos < 0)){
+					throw new Error("Active position undefined");
+				}
+
 				this.addArrows();
 				this.addSlidesToEnd();
-				this.setActiveSlide();
-				this.moveToActive();
+				this.setActiveSlide();	
 				this.arrowClickHandler();
 
-				inter = setInterval(this.actionInterval, settings.timeStep)	
+				inter = $slider.startTimer(inter);
 			}
 
 			this.initSlider();
